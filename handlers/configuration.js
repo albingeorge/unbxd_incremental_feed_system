49ee233baea4a231ea2d0280166705b450e9f7f7
Albin George <albinin0002@gmail.com>
2015-04-02 21:30:42 +0530

    Insert tho configs into redis

--- handlers/configuration.js
+++ handlers/configuration.js
@@ -5,9 +5,18 @@ var redis_client = require("../redis_client")["redisClient"];
 var set_configuration = function(req, res, next) {
     if(validate_config_elements(req.body)) {
         // Push the config for the site to redis and send status as success
+        console.log("Setting config for site " + req.params.site_name);
+        // Find a way to convert object to string
+        for (key in req.body) {
+            console.log("Key: " + "config:" + req.params.site_name + ":" + key);
+            console.log("Value: " + req.body[key]);
+            console.log();
+            redis_client.set("config:" + req.params.site_name + ":" + key, req.body[key]);
+        }
         res.status(200).send("Success");
+    } else {
+        res.status(400).send("Invalid config elements sent");
     }
-    res.status(400).send("Invalid config elements sent");
 }
 
 function validate_config_elements(body) {
